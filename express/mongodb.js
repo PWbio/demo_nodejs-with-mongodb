8c8f0f3aea624f51c691d1300eb16b0123eaf488
Po Wang <powangsept@gmail.com>
2021-10-14 23:16:08 +0800

    refactor: remove unused code section

--- express/mongodb.js
+++ express/mongodb.js
@@ -19,38 +19,4 @@ const compSchema = new mongoose.Schema({
 // compile schema into model (similar to class v.s. object)
 const Comp = mongoose.model("Company", compSchema); // return a class
 
-// POST
-const createComp = async (data) => {
-  let result;
-  if (Array.isArray(data)) {
-    // multiple document
-    result = await Comp.insertMany(data);
-  } else {
-    // single document
-    const comp = new Comp(data); // new document
-    result = await comp.save(); // upload document
-  }
-  return result;
-};
-
-// PUT
-const updateComp = async () => {
-  // Look up the course
-  // If not existing, return 404
-  const course = courses.find((c) => c.id === parseInt(req.params.id));
-  if (!course) return res.status(404).send("Not found");
-
-  // Validate
-  const { error } = validateCourse(req.body);
-
-  // If invalid, return 400 - Bad request
-  if (error) return res.status(400).send(error.details[0].message);
-
-  // Update course
-  course.name = req.body.name;
-
-  // Return the updated course
-  res.send(course);
-};
-
 module.exports = Comp;
